de20ea4b4e3416d6404f86f366070a7836155052
n-jay <nuwan.2015235@iit.ac.lk>
2017-07-21 12:08:04 +0530

    First commit

---
+++ src/main/java/org/wso2/device/mgt/plugins/App.java
@@ -0,0 +1,15 @@
+package org.wso2.device.mgt.plugins;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        String p = "<p>New Paragraph</p>";
+        System.out.println(StringEscapeUtils.escapeHtml4(p));
+    }
+}
