4a816593d8d4aa39d8ad5f8eed373405a788f6e3
n-jay <nuwan.2015235@iit.ac.lk>
2017-07-24 14:33:52 +0530

    Added jar file for MySQL database

--- src/main/java/org/wso2/device/mgt/plugins/App.java
+++ src/main/java/org/wso2/device/mgt/plugins/App.java
@@ -2,15 +2,44 @@ package org.wso2.device.mgt.plugins;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Hello world!
- *
+ * This is the main class of the program.
+ * Purpose is to create the GUI and handle major logic code
  */
-public class App 
-{
+
+public class App extends JFrame {
+
+    public static JFrame frame;
+    public static JPanel panel;
+    public static JLabel text;
+
+    //main method
     public static void main( String[] args ) {
-        String p = "<p>New Paragraph</p>";
-//        System.out.println(StringEscapeUtils.escapeHtml4(p));
-        System.out.println("HelloWorld");
+        new App();
     }
+
+    //constructor for the GUI app
+    public App() {
+        guiMain();
+    }
+
+    //primary method including all GUI components and logic
+    private void guiMain() {
+        frame = new JFrame("Virtual Gateway");
+        frame.setVisible(true);
+        frame.setSize(800,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(900, 420));
+        frame.setLocationRelativeTo(null);
+
+        panel = new JPanel();
+        text = new JLabel("Hello World");
+
+        panel.add(text);
+        frame.add(panel);
+    }
+
 }
